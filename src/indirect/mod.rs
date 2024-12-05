02c5654d5720c74170012a512f3045f50a22262d
Phil Weber <philweber@microsoft.com>
2024-12-05 15:42:18 -0800

    Changed entire function to mark as unsafe and added comment

--- src/indirect/mod.rs
+++ src/indirect/mod.rs
@@ -44,80 +44,81 @@ impl FfaIndirectMsg {
         FfaError::Ok
     }
 
-    pub fn read_indirect_msg(
+    /// # Safety
+    ///
+    /// This function directly reads physical memory pointed to by base_addr
+    pub unsafe fn read_indirect_msg(
         &self,
         base_addr: u64,
         seq_num: u16,
         buf: &mut [u8; QUEUE_BLOCK_SIZE],
     ) -> FfaError {
-        unsafe {
-            let asyncmsg = base_addr as *mut AsyncMsgHeader;
-            let entry_count = (*asyncmsg).count as usize;
-            let mut index = 0;
+        let asyncmsg = base_addr as *mut AsyncMsgHeader;
+        let entry_count = (*asyncmsg).count as usize;
+        let mut index = 0;
 
-            for b in &mut (*asyncmsg).bitmap {
-                let sn = b.seq_num;
-                if sn == seq_num && b.state == QUEUE_STATE_VALID {
-                    let length = b.length;
-                    ptr::copy_nonoverlapping(
-                        &((*asyncmsg).bitmap[index]) as *const AsyncMsgBitmap as *const u8,
-                        buf.as_mut_ptr(),
-                        length as usize,
-                    );
-                    b.length = 0;
-                    b.state = QUEUE_STATE_FREE;
-                    b.seq_num = 0;
-                    break;
-                }
-                index += 1;
+        for b in &mut (*asyncmsg).bitmap {
+            let sn = b.seq_num;
+            if sn == seq_num && b.state == QUEUE_STATE_VALID {
+                let length = b.length;
+                ptr::copy_nonoverlapping(
+                    &((*asyncmsg).bitmap[index]) as *const AsyncMsgBitmap as *const u8,
+                    buf.as_mut_ptr(),
+                    length as usize,
+                );
+                b.length = 0;
+                b.state = QUEUE_STATE_FREE;
+                b.seq_num = 0;
+                break;
             }
-
-            if index < entry_count {
-                return FfaError::Ok;
-            }
+            index += 1;
         }
 
+        if index < entry_count {
+            return FfaError::Ok;
+        }
         FfaError::Retry
     }
 
-    pub fn write_indirect_msg(&self, base_addr: u64, seq_num: u16, _buf: &[u8]) -> FfaError {
+    /// # Safety
+    ///
+    /// This function directly reads physical memory pointed to by base_addr
+    pub unsafe fn write_indirect_msg(&self, base_addr: u64, seq_num: u16, buf: &[u8]) -> FfaError {
         // Note if we have multi-threaded support we need to take a lock while accessing the queue
-        unsafe {
-            let asyncmsg = base_addr as *mut AsyncMsgHeader;
-            let entry_count = (*asyncmsg).count as usize;
-            let mut data_len = _buf.len();
-            let mut bm_index: usize = 0;
-            let mut data_index: usize = 0;
+        let asyncmsg = base_addr as *mut AsyncMsgHeader;
+        let entry_count = (*asyncmsg).count as usize;
+        let mut data_len = buf.len();
+        let mut bm_index: usize = 0;
+        let mut data_index: usize = 0;
 
-            for b in &mut (*asyncmsg).bitmap {
-                let state = b.state;
-                if state == QUEUE_STATE_FREE {
-                    // Copy data first then update the header structure
-                    let mut copy_len = QUEUE_BLOCK_SIZE;
-                    let data_base = base_addr + (QUEUE_BLOCK_SIZE * bm_index + 1) as u64;
-                    if data_len < copy_len {
-                        copy_len = data_len;
-                    }
-                    ptr::copy_nonoverlapping(&_buf[data_index], data_base as *mut u8, copy_len);
-                    data_index += copy_len;
-                    data_len -= copy_len;
-
-                    b.length = copy_len as u16;
-                    b.state = QUEUE_STATE_VALID;
-                    b.seq_num = seq_num;
-
-                    // After we've copied over all the data break
-                    if data_len == 0 {
-                        break;
-                    }
+        for b in &mut (*asyncmsg).bitmap {
+            let state = b.state;
+            if state == QUEUE_STATE_FREE {
+                // Copy data first then update the header structure
+                let mut copy_len = QUEUE_BLOCK_SIZE;
+                let data_base = base_addr + (QUEUE_BLOCK_SIZE * (bm_index + 1)) as u64;
+                if data_len < copy_len {
+                    copy_len = data_len;
                 }
-                bm_index += 1;
-            }
+                ptr::copy_nonoverlapping(&buf[data_index], data_base as *mut u8, copy_len);
+                data_index += copy_len;
+                data_len -= copy_len;
 
-            if bm_index < entry_count {
-                return FfaError::Ok;
+                b.length = copy_len as u16;
+                b.state = QUEUE_STATE_VALID;
+                b.seq_num = seq_num;
+
+                // After we've copied over all the data break
+                if data_len == 0 {
+                    break;
+                }
             }
-            FfaError::NoMemory
+            bm_index += 1;
         }
+
+        if bm_index < entry_count {
+            return FfaError::Ok;
+        }
+        FfaError::NoMemory
     }
 }
